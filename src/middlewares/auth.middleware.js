1921c515ee25c25c73e47b9c037bbcf482d10274
pg147 <xyzprathamesh147@gmail.com>
2025-01-21 23:06:20 +0530

    Auth middleware added for token verification

---
+++ src/middlewares/auth.middleware.js
@@ -0,0 +1,34 @@
+import { User } from "../models/user.model.js";
+import { asyncHandler } from "../utils/asyncHandler.js";
+import jwt from "jsonwebtoken";
+
+const verifyToken = asyncHandler(async (req, _, next) => {
+    try {
+        const token = req.cookies.accessToken || req.header("Authorization").replace("Bearer ", "");
+
+        if (!token) {
+            return res.status(401).json({
+                success: false,
+                message: "Unauthorized access!"
+            });
+        }
+
+        const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
+
+        const user = await User.findById(decodedToken?._id).select("-password -refreshToken");
+
+        if (!user) {
+            return res.status(400).json({
+                success: false,
+                message: "Invalid access token"
+            });
+        }
+
+        req.user = user;
+        next();
+    } catch (error) {
+        console.log("Error verifying token : ", error.message);
+    }
+})
+
+export { verifyToken };
