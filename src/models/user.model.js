862faea30a9afbce250386a200d4b5b248a7516a
pg147 <xyzprathamesh147@gmail.com>
2025-01-21 20:46:05 +0530

    .env variable fixed

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -70,7 +70,7 @@ userSchema.methods.generateAccessToken = function () {
         },
         process.env.ACCESS_TOKEN_SECRET,
         { 
-            expiresIn: process.env.ACCESS_TOKEN_SECRET_EXPIRY 
+            expiresIn: process.env.ACCESS_TOKEN_EXPIRY 
         }
     )
 }
@@ -83,7 +83,7 @@ userSchema.methods.generateRefreshToken = function () {
         },
         process.env.REFRESH_TOKEN_SECRET,
         { 
-            expiresIn: process.env.REFRESH_TOKEN_SECRET_EXPIRY 
+            expiresIn: process.env.REFRESH_TOKEN_EXPIRY 
         }
     )
 }
