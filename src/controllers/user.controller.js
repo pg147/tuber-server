31774af401e820a5c8b0a8a503297c1e02d56ae9
pg147 <xyzprathamesh147@gmail.com>
2025-01-21 20:46:25 +0530

    Login logic added in controller

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -2,6 +2,26 @@ import { User } from '../models/user.model.js';
 import { asyncHandler } from '../utils/asyncHandler.js';
 import { uploadOnCloudinary } from '../utils/cloudinary.js';
 
+const generateTokens = async (userId) => {
+    try {
+        const user = await User.findById(userId);
+        let accessToken;
+        let refreshToken;
+    
+        if (user) {
+            accessToken = await user.generateAccessToken();
+            refreshToken = await user.generateRefreshToken();
+
+            user.refreshToken = refreshToken;
+            await user.save();
+        }
+        
+        return { accessToken, refreshToken };
+    } catch (error) {
+        console.log("Error generating tokens : ", error.message);
+    }
+}
+
 const createUser = asyncHandler(async (req, res) => {
     const { username, fullName, email, password } = req.body;
 
@@ -37,6 +57,7 @@ const createUser = asyncHandler(async (req, res) => {
         coverLocalPath = req.files?.coverImage[0]?.path;
     }
 
+    // Uploading images on Cloudinary
     const avatar = avatarLocalPath ? await uploadOnCloudinary(avatarLocalPath) : "";
     const coverImage = coverLocalPath ? await uploadOnCloudinary(coverLocalPath) : "";
 
@@ -68,4 +89,55 @@ const createUser = asyncHandler(async (req, res) => {
     })
 });
 
-export { createUser };
+const loginUser = asyncHandler(async (req, res) => {
+    const { email, password } = req.body;
+
+    // Validations to check if both email and password are provided
+    if (!email || !password) {
+        return res.status(400).json({
+            success: false,
+            message: 'Both email and password must be provided!'
+        });
+    }
+
+    // Checking if the user exists in Database
+    const user = await User.findOne({ $or: [{ email }, { password }] });
+
+    // If no user exists
+    if (!user) {
+        return res.status(404).json({
+            success: false,
+            message: `User with ${email} doesn't exist`,
+        });
+    }
+
+    // Checking password with schema method
+    const checkPassword = await user.isPasswordCorrect(password);
+
+    // If password check fails
+    if (!checkPassword) {
+        return res.status(400).json({
+            success: false,
+            message: 'Invalid password! try again.'
+        });
+    }
+
+    // Generating tokens
+    const tokens = await generateTokens(user._id);
+
+    // If token creation fails
+    if (!tokens) {
+        return res.status(400).json({
+            success: false,
+            message: 'Error generating tokens!'
+        });
+    }
+
+    return res.status(200).json({
+        success: true,
+        message: `${email} logged in successfully!`,
+        tokens: tokens
+    })
+});
+
+export { createUser, loginUser };
