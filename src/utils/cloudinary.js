b3c8699eb108bce269ad0729c42ae15f38d27093
pg147 <xyzprathamesh147@gmail.com>
2025-01-21 17:22:04 +0530

    dotenv issues fixed for config

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -1,11 +1,14 @@
 import { v2 as cloudinary } from "cloudinary";
 import fs from "fs";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 // Configuration
 cloudinary.config({
-    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-    api_key: process.env.CLOUDINARY_API_KEY,
-    api_secret: process.env.CLOUDINARY_API_SECRET
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME.toString(),
+    api_key: process.env.CLOUDINARY_API_KEY.toString(),
+    api_secret: process.env.CLOUDINARY_API_SECRET.toString()
 });
 
 const uploadOnCloudinary = async (localFilePath) => {
